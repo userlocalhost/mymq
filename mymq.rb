0d46a7ab74c1cd261ce879c9c62378cb155ebd7f
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2015-12-11 19:59:08 +0900

    implement e2e_exchange feature

--- mymq.rb
+++ mymq.rb
@@ -16,14 +16,14 @@ module MyMQ
 
   def self.send(data, name, rkey)
     channel_open do |ch|
-      x = ch.direct(name)
+      x = ch.topic(name)
       x.publish(data, :routing_key => rkey)
     end
   end
 
   def self.receive(name, rkey, &block)
     channel_open do |ch|
-      x = ch.direct(name)
+      x = ch.topic(name)
       
       q = ch.queue('', :exclusive => true)
       q.bind(x, :routing_key => rkey)
@@ -33,6 +33,22 @@ module MyMQ
           block.call(body)
         end
       rescue Exception => _
+      end
+    end
+  end
+
+  def self.bridge(src, dst, rkey)
+    channel_open do |ch|
+      x1 = ch.topic(src)
+      x2 = ch.topic(dst)
+
+      x1.bind(x2, :routing_key => rkey)
+
+      begin
+        while true do
+          # nope
+        end
+      rescue Exception => e
       end
     end
   end
